fae56e6588ad6264a48666ddaf4b5f038180c9d3
vicky <vignesh@crisoconsulting.in>
2018-11-08 12:06:52 +0530

    8/11 commit

--- mlm/mlm/customer_tree.py
+++ mlm/mlm/customer_tree.py
@@ -10,24 +10,32 @@ from frappe import _
 # from frappe.utils.nestedset import NestedSet, update_nsm
 
 
-
 @frappe.whitelist()
 def get_children(doctype, parent=None, customer=None, is_root=False):
 	if parent is None or parent == "All Customers":
 		parent = ""
 
-	return frappe.db.sql("""
-		select
-			name as value,
-			is_group as expandable
-		from
-			`tab{doctype}` comp
-		where
-			ifnull(parent_customer, "")={parent}
-		""".format(
-			doctype=doctype,
-			parent=frappe.db.escape(parent)
-		), as_dict=1)
+
+	fields = ['name as value', 'is_group as expandable']
+	filters = [
+		['ifnull(`parent_customer`, "")', '=', parent]
+	]
+
+	customers = frappe.get_list(doctype, fields=fields, filters=filters, order_by='name')
+
+	return customers
+	# return frappe.db.sql("""
+	# 	select
+	# 		name as value,
+	# 		is_group as expandable
+	# 	from
+	# 		`tab{doctype}` comp
+	# 	where
+	# 		ifnull(parent_customer, "")={parent}
+	# 	""".format(
+	# 		doctype=doctype,
+	# 		parent=frappe.db.escape(parent)
+	# 	), as_dict=1)
 
 @frappe.whitelist()
 def add_node():
