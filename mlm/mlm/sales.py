441328609efb7a1460c3433f10659f3774357452
vicky <vignesh@crisoconsulting.in>
2018-11-11 15:24:15 +0530

    a

--- mlm/mlm/sales.py
+++ mlm/mlm/sales.py
@@ -100,13 +100,5 @@ def validate():
 		'user_remark': 'Referal bonus.'
 		}).insert(ignore_permissions=True)
 		
-		# adoc.flags.ignore_permissions = True
-		# adoc.submit()
 
-
-		# si = frappe.get_doc("Sales Invoice", x.name)
-		# si.jv_no = adoc.name
-		# si.mlm_jv = 1
-		# si.flags.ignore_permissions = True
-		# si.save(ignore_permissions=True)
-	return adoc
+	return row
